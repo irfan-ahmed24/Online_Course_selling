6a7f53543a117f6c6f665e9bfd7a19bba39db68d
irfan-ahmed <irfanahmed99989@gmail.com>
2026-07-30 22:55:07 +0600

    first commint

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Online_Course_selling.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
